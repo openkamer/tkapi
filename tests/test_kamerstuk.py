e834d10a77ac175efb6b946950261dece2333093
Bart Römgens <bart.romgens@gmail.com>
2017-10-26 16:43:44 +0200

    Create kamerstuk item

---
+++ tests/test_kamerstuk.py
@@ -0,0 +1,38 @@
+import datetime
+import unittest
+
+from tkapi import api
+from tkapi.kamerstuk import Kamerstuk
+from tkapi.kamerstuk import KamerstukFilter
+
+
+class TestKamerstuk(unittest.TestCase):
+
+    def test_get_kamerstuk(self):
+        ks_uid = '8d5481b7-d6b9-4452-921d-003819845c48'
+        kamerstuk = api.get_item(Kamerstuk, ks_uid)
+        self.assertEqual(kamerstuk.id, ks_uid)
+        self.assertEqual(kamerstuk.ondernummer, '82')
+
+    def test_get_kamerstukken(self):
+        kamerstukken = api.get_kamerstukken(filter=None, max_items=100)
+        for kamerstuk in kamerstukken:
+            kamerstuk.print_json()
+
+    def test_get_kamerstuk_by_ondernummer(self):
+        ondernummer = '82'
+        kamerstuk_filter = KamerstukFilter()
+        kamerstuk_filter.filter_ondernummer(ondernummer)
+        kamerstukken = api.get_kamerstukken(filter=kamerstuk_filter)
+        self.assertTrue(len(kamerstukken) > 100)
+        kamerstukken[0].print_json()
+        for kamerstuk in kamerstukken:
+            self.assertEqual(kamerstuk.ondernummer, ondernummer)
+
+    def test_get_kamerstuk_parlementair_document(self):
+        ks_uid = '8d5481b7-d6b9-4452-921d-003819845c48'
+        kamerstuk = api.get_item(Kamerstuk, ks_uid)
+        self.assertEqual(kamerstuk.id, ks_uid)
+        self.assertEqual(kamerstuk.ondernummer, '82')
+        pd = kamerstuk.parlementair_document
+        pd.print_json()
---
+++ tkapi/kamerstuk.py
@@ -0,0 +1,40 @@
+import json
+
+import tkapi
+
+from tkapi.document import ParlementairDocument
+from tkapi.dossier import Dossier
+
+
+class KamerstukFilter(tkapi.Filter):
+
+    def __init__(self):
+        super().__init__()
+
+    def filter_ondernummer(self, ondernumer):
+        filter_str = "Ondernummer eq " + "'" + str(ondernumer) + "'"
+        self.filters.append(filter_str)
+
+
+class Kamerstuk(tkapi.TKItem):
+    url = 'Kamerstuk'
+    expand_param = 'Kamerstukdossier, ParlementairDocument'
+
+    def __init__(self, kamerstuk_json):
+        super().__init__(kamerstuk_json)
+
+    @property
+    def ondernummer(self):
+        return self.get_property_or_empty_string('Ondernummer')
+
+    @property
+    def parlementair_document(self):
+        self.print_json()
+        pd_uid = self.get_property_or_none('ParlementairDocument')['Id']
+        print(pd_uid)
+        return tkapi.api.get_item(ParlementairDocument, pd_uid)
+
+    @property
+    def dossier(self):
+        dossier_uid = self.get_property_or_none(Dossier.url)['Id']
+        return tkapi.api.get_item(Dossier, dossier_uid)
