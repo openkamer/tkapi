6f212117d906efede4f08d414477d08cd52a0ee8
Bart Römgens <bart.romgens@gmail.com>
2017-10-26 15:09:28 +0200

    Create parlementair document test

---
+++ tests/test_document.py
@@ -0,0 +1,33 @@
+import datetime
+import unittest
+
+from tkapi import api
+from tkapi.document import ParlementairDocumentFilter
+
+
+class TestParlementairDocument(unittest.TestCase):
+    start_datetime = datetime.datetime(year=2017, month=1, day=1)
+    end_datetime = datetime.datetime(year=2017, month=6, day=1)
+
+    def test_get_voorstel_van_wet(self):
+        pd_filter = ParlementairDocumentFilter()
+        pd_filter.filter_date_range(
+            TestParlementairDocument.start_datetime,
+            TestParlementairDocument.end_datetime
+        )
+        pd_filter.filter_soort('Voorstel van wet')
+        # print(pd_filter.filter_str)
+        pds = api.get_parlementaire_documenten(pd_filter)
+        for pd in pds:
+            print(pd.title)
+            print(pd.activiteit)
+            # print('\t' + str(pd.dossier.vetnummer))
+            # print('\t dossier afgesloten: ' + str(pd.dossier.afgesloten))
+            # pd.dossier.print_json()
+            # print(pd.dossier.afgesloten)
+            # print(pd.dossier.kamerstukken)
+            # for zaak in pd.dossier.zaken:
+            #     print('\t zaak afgedaan: ' + str(zaak.afgedaan))
+            #     print('\t zaak soort: ' + str(zaak.soort))
+            #     print('\t zaak soort: ' + str(zaak.nummer))
+
