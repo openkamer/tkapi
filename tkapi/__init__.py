b1b81bf98cead0aa6fa013ff5d29a2ccc638a0f1
Bart Römgens <bart.romgens@gmail.com>
2017-10-26 15:08:26 +0200

    Create api on import

--- tkapi/__init__.py
+++ tkapi/__init__.py
@@ -1,3 +1,8 @@
 from .core import TKItem
+from .filter import Filter
+from .filter import SoortFilter
 from .api import Api
 from . import util
+
+from local_settings import USER, PASSWORD
+api = Api(user=USER, password=PASSWORD, verbose=True)
