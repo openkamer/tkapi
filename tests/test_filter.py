33c1353de82bd9f1175ea85c90a106645246aa3b
Bart Römgens <bart.romgens@gmail.com>
2017-11-04 20:54:49 +0100

    Create filter mixin test

---
+++ tests/test_filter.py
@@ -0,0 +1,12 @@
+import unittest
+
+from tkapi.document import ParlementairDocumentFilter
+
+
+class TestFilters(unittest.TestCase):
+
+    def test_filter_mixin(self):
+        pd_filter = ParlementairDocumentFilter()
+        pd_filter.filter_soort('test soort')
+        pd_filter.filter_empty_zaak()
+        self.assertEqual(len(pd_filter.filters, 2))
