fa73e48f873031e436a827352e2b9f8dcfd9ac1f
Bart Römgens <bart.romgens@gmail.com>
2017-11-04 20:58:52 +0100

    Fix filter test

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -9,4 +9,4 @@ class TestFilters(unittest.TestCase):
         pd_filter = ParlementairDocumentFilter()
         pd_filter.filter_soort('test soort')
         pd_filter.filter_empty_zaak()
-        self.assertEqual(len(pd_filter.filters, 2))
+        self.assertEqual(len(pd_filter.filters), 2)
