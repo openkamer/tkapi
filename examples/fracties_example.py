dc7b49d59a30e7989f4126b32efcd957969651d8
Bart Römgens <bart.romgens@gmail.com>
2018-10-13 12:09:33 +0200

    Add fracties example

---
+++ examples/fracties_example.py
@@ -0,0 +1,22 @@
+import tkapi
+from tkapi.actor import Fractie
+
+from local_settings import USER, PASSWORD
+
+api = tkapi.Api(user=USER, password=PASSWORD, verbose=True)
+
+
+def example_fracties_leden_actief():
+    """Example that shows how to get all active fracties and their active members."""
+    filter = Fractie.create_filter()
+    filter.filter_actief()
+    fracties_actief = api.get_fracties(filter=filter)
+    for fractie in fracties_actief:
+        print('{} ({}) ({} zetels)'.format(fractie.naam, fractie.afkorting, fractie.zetels))
+        leden = fractie.leden_actief
+        for lid in leden:
+            print('  ', lid.persoon, lid.van)
+
+
+if '__main__' == __name__:
+    example_fracties_leden_actief()
