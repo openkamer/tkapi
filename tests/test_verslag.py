3399f2cd5f8b58cdf7a805069220aff450f28816
Bart Römgens <bart.romgens@gmail.com>
2018-10-20 12:10:34 +0200

    Add tests for vergadering and verslag

--- tests/test_verslag.py
+++ tests/test_verslag.py
@@ -3,6 +3,37 @@ import datetime
 from tkapi.verslag import VerslagAlgemeenOverleg
 
 from .core import TKApiTestCase
+from tkapi.verslag import Verslag
+from tkapi.info import get_verslag_soorten
+
+
+class TestVerslag(TKApiTestCase):
+    n_items = 10
+
+    def test_get_verslag(self):
+        uid = '28f7d21d-79f1-4591-a0f7-64bb13996a05'
+        verslag = self.api.get_item(Verslag, id=uid)
+        self.assertEqual('Eindpublicatie', verslag.soort)
+        self.assertEqual('Gecorrigeerd', verslag.status)
+        self.assertIsNotNone(verslag.vergadering)
+
+    def test_get_verslagen(self):
+        verslagen = self.api.get_verslagen(max_items=self.n_items)
+        self.assertEqual(self.n_items, len(verslagen))
+
+    def test_verslag_filter_soort(self):
+        soort = 'Eindpublicatie'
+        filter = Verslag.create_filter()
+        filter.filter_soort(soort)
+        verslagen = self.api.get_verslagen(filter=filter, max_items=self.n_items)
+        self.assertEqual(self.n_items, len(verslagen))
+        for verslag in verslagen:
+            self.assertEqual(soort, verslag.soort)
+
+    # def test_get_soorten(self):
+    #     soorten = get_verslag_soorten()
+    #     for soort in soorten:
+    #         print(soort)
 
 
 class TestVerslagAlgemeenOverleg(TKApiTestCase):
