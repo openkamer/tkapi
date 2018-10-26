69645c277c603988a1dd82e235f657d6242bbb7d
Bart Römgens <bart.romgens@gmail.com>
2018-10-26 19:31:26 +0200

    Add reizen example

---
+++ examples/reizen_fractieleden_example.py
@@ -0,0 +1,20 @@
+import tkapi
+from tkapi.util import queries
+
+from local_settings import USER, PASSWORD
+
+api = tkapi.Api(user=USER, password=PASSWORD, verbose=False)
+
+
+def example_reizen_fractieleden_actief():
+    leden_actief = queries.get_fractieleden_actief()
+    for lid in leden_actief:
+        persoon = lid.persoon
+        print('{} ({})'.format(persoon, lid.fractie.afkorting))
+        reizen = lid.persoon.reizen
+        for reis in reizen:
+            print('\t', reis.bestemming, '|', reis.doel, '|', reis.van)
+
+
+if '__main__' == __name__:
+    example_reizen_fractieleden_actief()
