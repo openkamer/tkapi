dfc2618b810bc0f0c587a5fdefd4df5ee7dbcaf9
Bart Römgens <bart.romgens@gmail.com>
2019-04-05 22:54:23 +0200

    Bump dev version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='tkapi',
     description='Python bindings for the Tweede Kamer OData API',
     long_description=long_description,
-    version='0.7.2',
+    version='0.8.0.dev1',
     url='https://github.com/openkamer/tkapi',
     author='Open Kamer',
     author_email='info@openkamer.org',
